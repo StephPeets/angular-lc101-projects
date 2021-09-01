43fa5446ac1b3751eeb336b054f34c10803ab085
Steph Peets <stephanie.peets@gmail.com>
2021-08-31 21:42:50 -0400

    bonus missions complete

--- lesson2/examples/ngfor-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/ngfor-practice/src/app/movie-list/movie-list.component.ts
@@ -7,17 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
   movies = ['Paris is Burning', 'A Dog Day Afternoon', 'Lost in Translation', "Bob's Burgers", 'Coming to America'];
-  
+
   constructor() { }
 
   ngOnInit() {
   }
-alertFunction(){
-  alert("MOUSEOVER")
-}
-addMovie(movie: string) {
-  if(!this.movies.includes(movie) && movie != '') {
+
+  addMovie(movie: string) {
+    if (movie === '') {
+      alert("Please enter a movie.");
+    } else if (this.movies.includes(movie)) {
+      alert("Movie already on list.")
+    } else {
       this.movies.push(movie);
+    }
   }
-}
 }
