1b8570b29b70f5dc5f90d3fcb468a4b04874b64e
Steph Peets <stephanie.peets@gmail.com>
2021-09-03 19:16:30 -0400

    starting exercises for ch. 30

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,38 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+
+  // movement = parseInt(img.rocketImage.style.left) + 10 + 'px'; THIS IS WHERE YOU LEFT OFF
+
+
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+       this.color = 'blue';
+       this.height = 10000;
+       this.width = 0;
+       this.message = 'Shuttle in flight.';
+    }
+ }
+
+ handleLanding() {
+  // let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+  // if (result) {
+     this.color = 'green';
+     this.height = 0;
+     this.width = 0;
+     this.message = 'Shuttle has safely landed.';
+  // }
+}
+
+ handleAbortMission() {
+  let result = window.confirm('Are you sure you want to abort this mission?');
+  if (result) {
+     this.color = 'darkRed';
+     this.height = 0;
+     this.width = 0;
+     this.message = `Mission aborted. Note: Houston was of no help.`;
+  }
+}
+
 }
